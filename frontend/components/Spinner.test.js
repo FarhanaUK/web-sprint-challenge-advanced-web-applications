77c760ab237677c968561687ce9111e95f677ad0
Farhana Aktar <farhanaaktar@live.co.uk>
2024-10-07 12:38:44 +0100

    tests passed

--- frontend/components/Spinner.test.js
+++ frontend/components/Spinner.test.js
@@ -1,5 +1,19 @@
 // Import the Spinner component into this file and test
 // that it renders what it should for the different props it can take.
-test('sanity', () => {
-  expect(true).toBe(false)
+import React from "react"
+import { render, screen } from "@testing-library/react"
+import Spinner from "./Spinner"
+
+
+describe("Spinner Component", () => {
+  test("That the spinner component renders on props true", () => {
+    render(<Spinner on={true} />);
+    expect(screen.queryAllByText(/Please wait.../i)).not.toBeNull();
+  });
+test('does not render the spinner when "on" is false', () => {
+  
+render(<Spinner on={false} />)
+  expect(screen.queryAllByText(/Please wait.../i)).toHaveLength(0);
 })
+})
+
